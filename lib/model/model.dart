035a89e2b34a81da9632b4eae34b3f4429b0ce1e
Phuchuynh1452 <19004158@st.vlute.edu.vn>
2023-02-19 07:48:47 +0700

    add model model.dart and update testcomit.dart

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,2 +1,14 @@
 late int id;
 late String na;
+late int id1;
+late String na1;
+late int id2;
+late String na2;
+late int id3;
+late String na3;
+late int id4;
+late String na4;
+late int id5;
+late String na5;
+
+
